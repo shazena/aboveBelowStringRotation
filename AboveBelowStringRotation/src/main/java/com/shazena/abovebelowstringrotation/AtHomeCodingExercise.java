9b52ac731b98bc955081a6f0342821772051c2da
shazena <shazena93@gmail.com>
2022-02-06 16:21:30 -0500

    add Validation Exception for aboveBelow

--- AboveBelowStringRotation/src/main/java/com/shazena/abovebelowstringrotation/AtHomeCodingExercise.java
+++ AboveBelowStringRotation/src/main/java/com/shazena/abovebelowstringrotation/AtHomeCodingExercise.java
@@ -2,6 +2,8 @@ package com.shazena.abovebelowstringrotation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,13 +15,18 @@ public class AtHomeCodingExercise {
 
         ArrayList<Integer> testingList = new ArrayList<>();
 
+//        testingList = null;
         testingList.add(1);
         testingList.add(5);
         testingList.add(2);
         testingList.add(1);
         testingList.add(10);
 
-        System.out.println(aboveBelow(testingList, 6));
+        try {
+            System.out.println(aboveBelow(testingList, 6));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
     }
 
@@ -31,8 +38,11 @@ public class AtHomeCodingExercise {
      * @return HashMap<String, Integer> of the format {"above": int, "below":
      * int}
      */
-    public static HashMap<String, Integer> aboveBelow(ArrayList<Integer> list, int comparisonVal) {
+    public static HashMap<String, Integer> aboveBelow(ArrayList<Integer> list, int comparisonVal) throws Exception {
 
+        if (list == null) {
+            throw new Exception("Validation Error");
+        }
         HashMap<String, Integer> result = new HashMap<>();
 
         int above = 0;
